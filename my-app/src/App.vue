a4e263f636a1b11b00ed4f1cfdfdbea8b9579b9f
Daniele Oliveira <droliver.dev@gmail.com>
2020-04-04 14:14:49 -0300

    vue app

---
+++ my-app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    getTest() {
+      return Vue.http.get('http://localhost:8000/api/teste').then(response => {
+        console.log(response)
+      }, err => {
+        console.log(err)
+      })
+    }
+  },
+  created () {
+    this.getTest();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
